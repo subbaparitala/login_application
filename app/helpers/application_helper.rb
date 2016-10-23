c54721e6213413b7278f4123db2952032294669f
subbaparitala <subbaparitala@gmail.com>
2016-10-23 00:30:29 -0500

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def full_title(page_title)                          # Method definition
+    base_title = "Ruby on Rails Login Application"  # Variable assignment
+    if page_title.empty?                              # Boolean test
+      base_title                                      # Implicit return
+    else
+      "#{page_title} | #{base_title}"                 # String interpolation
+    end
+  end
 end
+
